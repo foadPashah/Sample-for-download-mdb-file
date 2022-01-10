0724e5c06c9eaa384f0a112fea4f7177f8d5b9f2
foad pashah <neo_2000matrix@yahoo.com>
2022-01-10 15:54:01 +0330

    Add project files.

---
+++ DownloadAccessFileSample/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using DownloadAccessFileSample.Utilities;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Mvc;
+
+namespace DownloadAccessFileSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public FileResult DownloadFile()
+        {
+            string oledbPath = OledbConnectionString.GetOledbConnection();
+            //fileContent = GetFile(oledbPath);
+            var fs = new FileStream(oledbPath, FileMode.Open);
+            return File(fs, "application/octet-stream", "Test.mdb");
+        }
+    }
+}
---
+++ DownloadAccessFileSample/Utilities/OledbConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+
+
+namespace DownloadAccessFileSample.Utilities
+{
+    public static class OledbConnectionString
+    {
+        public static string GetOledbConnection()
+        {
+            return ConfigurationManager.AppSettings["oledbConnection"];
+        }
+    }
+}
